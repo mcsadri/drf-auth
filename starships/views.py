7614335ebfc69ff03b8a9a009aebe6efb9a30f59
Manuch Sadri <colonquebang@hotmail.com>
2023-06-28 23:33:57 -0700

    updates urls and views, and adds serializer

--- starships/views.py
+++ starships/views.py
@@ -1,1 +1,13 @@
 from rest_framework import generics
+from .serializers import StarshipSerializer
+from .models import Starship
+
+
+class StarshipList(generics.ListAPIView):
+    queryset = Starship.objects.all()
+    serializer_class = StarshipSerializer
+
+
+class StarshipDetail(generics.RetrieveAPIView):
+    queryset = Starship.objects.all()
+    serializer_class = StarshipSerializer
